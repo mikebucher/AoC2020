4fdf5347106894c06d2dbb455a6b88edeaa2ee90
Mike Bucher <mike.bucher@tradefact.com>
2020-12-01 08:52:55 +0000

    day1 done

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace Day1
     {
         static void Main(string[] args)
         {
+            string strReadFile = @"C:\Dev\AoC2020\Day1.txt";
+            int result = 0;
+
+            var fileLines = File.ReadAllLines(strReadFile).ToList();
+
+            List<int> numbers = new List<int>();
+
+            foreach (var item in fileLines)
+            {
+                numbers.Add(Convert.ToInt32(item));
+            }
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 1; j < (numbers.Count - 1); j++)
+                {
+                    for (int k = 2; k < numbers.Count - 2; k++)
+                    {
+                        if(i != j && i != k && j != k)
+                        {
+                            if((numbers[i] + numbers[j] + numbers[k]) == 2020)
+                            {
+                                result = numbers[i] * numbers[j] * numbers[k];
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Result: " + result.ToString());
+            Console.ReadKey();
         }
     }
 }
